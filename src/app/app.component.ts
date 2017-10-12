fa464da014a0bcfdce7c4e4f0dcb1a9b670aeb22
Chandratre <pchandratre@rochester.inetgleason.com>
2017-10-12 11:48:28 -0400

    Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Yo maan, this be heroes';
+  selectedHero: Hero;
+  heroes = HEROES;
+
+  onSelect(hero:Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+const HEROES: Hero[] =[
+  { id:11, name: 'Spiderman'},
+  {id:12, name:'Superman'},
+  {id:13, name:'WonderWoman'},
+];
